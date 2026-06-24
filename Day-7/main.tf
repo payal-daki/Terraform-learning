f7303a59dea71c2b3ff5148bd39eccf06336baf3
payal-daki <payaldaki10@gmail.com>
2026-06-24 01:52:34 +0000

    update main.tf.

--- Day-7/main.tf
+++ Day-7/main.tf
@@ -1,4 +1,4 @@
-# Simple EC2 for Day 7 demo
+# Simple EC2 for Day 7 demo github action
 resource "aws_instance" "web" {
   ami           = "ami-0f58b397bc5c1f2e8"
   instance_type = var.instance_type
